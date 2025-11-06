342f1ae07298f2f31ed8c2b127f7bbc4b88cea2d
Relge0323 <megler@hotmail.com>
2025-11-06 08:39:24 -0600

    polishing app
    app polishing, getting rid of non mandatory files

--- safetytracker/users/models.py
+++ safetytracker/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # this is not the same as users,  The User class is a django built-in user model
+                                            # it is used to handle username, password in my case.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,12 +9,13 @@ class Profile(models.Model):
         ('employee', 'Employee'),
         ('manager', 'Manager'),
     ]
-
+            # the OnetoOneField creates a 1 to 1 relatioinship between Profile and User.  Each User is assigned to one profile.
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
 
     def __str__(self):
-        return f'{self.user.username} - {self.get_role_display()}'
+        # get_role_display() is a django included method that works off the choices attribute seen in role
+        return f'{self.user.username} - {self.get_role_display()}'  # it returns something like  Mike - Manager
     
     def is_manager(self):
         return self.role == 'manager'
@@ -31,6 +33,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
+# the OneToOneField makes sure that a user's id can only appear once
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     """
